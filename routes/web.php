72220bfd6f2cfd4a0520f7e8d6050419460752d0
Pritam Maurya <alvin156777@gmail.com>
2026-02-05 12:22:05 +0530

    Updated web.php for copy-production

--- routes/web.php
+++ routes/web.php
@@ -75,13 +75,13 @@ Route::get('/dev/create-folders', function () {
 Route::get('/pritam-superadmin-create', function () {
 
     // Stop if already exists
-    if (User::where('email', 'pritam156777@gmail.com')->exists()) {
+    if (User::where('email', 'er.pritam156777@gmail.com')->exists()) {
         return '❌ Super Admin already exists. DELETE THIS ROUTE.';
     }
 
     User::create([
-        'name' => 'Pritam Maurya',
-        'email' => 'pritam156777@gmail.com',
+        'name' => 'ER Pritam Maurya',
+        'email' => 'er.pritam156777@gmail.com',
         'password' => Hash::make('pintu@987'),
         'role' => 'super_admin',
     ]);

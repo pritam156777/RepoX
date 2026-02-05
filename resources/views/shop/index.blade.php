d195da0ab5a8a9aa82bf356279a59c05a8a6c80b
pritam156777 <48950709+pritam156777@users.noreply.github.com>
2026-02-05 21:21:21 +0530

    Refactor category title and image display logic

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -7,20 +7,22 @@
     <div class="views-shop-index-wrapper max-w-7xl mx-auto px-4 py-24">
 
 
-    {{-- CATEGORY TITLE --}}
-        <h1 class="text-3xl font-bold mb-4">
-            {{ $category ? $category->name : 'Shop' }}
-        </h1>
-
-        {{-- CATEGORY IMAGE --}}
-        @if($category && $category->photo)
-            <img src="{{ asset('storage/' . $category->photo) }}"
-                 class="h-40 rounded-lg mb-4">
-        @endif
-
-
+        
         @auth
             @if(auth()->user()->role === 'super_admin')
+
+
+                <h1 class="text-3xl font-bold mb-4">
+                    {{ $category ? $category->name : 'Shop' }}
+                </h1>
+
+                {{-- CATEGORY IMAGE --}}
+                @if($category && $category->photo)
+                    <img src="{{ asset('storage/' . $category->photo) }}"
+                         class="h-40 rounded-lg mb-4">
+                @endif
+
+
 
                 <form style="margin-bottom: 5pc" action="{{ route('categories.photo.update', $category->uuid) }}"
                       method="POST"
@@ -40,6 +42,7 @@
             @endif
         @endauth
 
+    
         
         
         @if($products->count())

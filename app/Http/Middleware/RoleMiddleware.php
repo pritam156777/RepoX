87fc06a4315890536c865064b47ba513e0273554
pritam156777 <48950709+pritam156777@users.noreply.github.com>
2026-02-05 16:36:48 +0530

    Fix debug output for roles in RoleMiddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -10,7 +10,7 @@ class RoleMiddleware
 {
   public function handle(Request $request, Closure $next, ...$roles)
     {
-        dd('$roles');
+        dd($roles);
         if (!auth()->check()) {
             abort(403);
         }
